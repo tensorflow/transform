Fix covariance and per-key accumulators encoding. Covariance is fixed by manually converting numpy types. Per-key encoding is fixed by reusing some of the vocabulary coder logic. This change also fixes the Covariance and PCA combiners which didn't previously cast their outputs to the requested dtype which would break BindTensors.
PiperOrigin-RevId: 267413673

tensorflow_transform/analyzer_nodes.py
@@ -196,10 +196,22 @@ class CacheCoder(object):
 
 
 class JsonNumpyCacheCoder(CacheCoder):
+  """An accumulator cache coder that can handle lists."""
+
+  def _convert_numpy_dtype(self, x):
+    if hasattr(x, 'tolist'):
+      return x.tolist()
+    return x
 
   def encode_cache(self, accumulator):
+    if isinstance(accumulator, (list, tuple)):
+      primitive_accumulator = [
+          self._convert_numpy_dtype(a) for a in accumulator
+      ]
+    else:
+      primitive_accumulator = self._convert_numpy_dtype(accumulator)
     # Need to wrap in np.array and call tolist to make it JSON serializable.
-    return tf.compat.as_bytes(json.dumps(np.array(accumulator).tolist()))
+    return tf.compat.as_bytes(json.dumps(primitive_accumulator))
 
   def decode_cache(self, encoded_accumulator):
     return np.array(json.loads(tf.compat.as_text(encoded_accumulator)))
@@ -331,7 +343,28 @@ class CacheableCombineMerge(
     return self.combiner.output_tensor_infos()
 
 
-class CacheableCombinePerKeyAccumulate(CacheableCombineAccumulate):
+class _CombinerPerKeyAccumulatorCoder(CacheCoder):
+  """Coder for per-key combiner accumulators."""
+
+  def __init__(self, value_coder):
+    self._combiner_coder = value_coder
+    self._vocabulary_coder = _BaseKVCoder()
+
+  def encode_cache(self, accumulator):
+    key, value = accumulator
+    encoded_value = self._combiner_coder.encode_cache(value)
+    return self._vocabulary_coder.encode_cache((key, encoded_value))
+
+  def decode_cache(self, encoded_accumulator):
+    accumulator = self._vocabulary_coder.decode_cache(encoded_accumulator)
+    key, encoded_value = accumulator
+    value = self._combiner_coder.decode_cache(encoded_value)
+    return (key, value)
+
+
+class CacheableCombinePerKeyAccumulate(
+    collections.namedtuple('CacheableCombineAccumulate', ['combiner', 'label']),
+    AnalyzerDef):
   """An analyzer that runs `beam.CombinePerKey` to accumulate without merging.
 
   This analyzer reduces the values that it accepts as inputs, using the
@@ -353,8 +386,22 @@ class CacheableCombinePerKeyAccumulate(CacheableCombineAccumulate):
     return super(CacheableCombinePerKeyAccumulate, cls).__new__(
         cls, combiner=combiner, label=label)
 
+  @property
+  def num_outputs(self):
+    return 1
 
-class CacheableCombinePerKeyMerge(CacheableCombineMerge):
+  @property
+  def is_partitionable(self):
+    return True
+
+  @property
+  def cache_coder(self):
+    return _CombinerPerKeyAccumulatorCoder(self.combiner.accumulator_coder)
+
+
+class CacheableCombinePerKeyMerge(
+    collections.namedtuple('CacheableCombineMerge', ['combiner', 'label']),
+    AnalyzerDef):
   """An analyzer that runs `beam.CombinePerKey` to only merge accumulators.
 
   This analyzer reduces the values that it accepts as inputs, using the
@@ -455,13 +502,36 @@ class VocabularyAccumulate(
     return _VocabularyAccumulatorCoder(input_dtype=self.input_dtype)
 
 
-class _VocabularyAccumulatorCoder(CacheCoder):
+class _BaseKVCoder(CacheCoder):
+  """Coder for key-value based accumulators."""
+
+  def __init__(self):
+    self._lengths_prefix_format = 'qq'
+    self._lengths_prefix_length = struct.calcsize(self._lengths_prefix_format)
+
+  def encode_cache(self, accumulator):
+    token, value = accumulator
+    len_token, len_value = len(token), len(value)
+    return struct.pack(
+        '{}{}s{}s'.format(self._lengths_prefix_format, len_token, len_value),
+        len_token, len_value, token, value)
+
+  def decode_cache(self, encoded_accumulator):
+    (len_token, len_value) = struct.unpack_from(
+        self._lengths_prefix_format,
+        encoded_accumulator[:self._lengths_prefix_length])
+    accumulator = struct.unpack_from(
+        '{}s{}s'.format(len_token, len_value),
+        encoded_accumulator[self._lengths_prefix_length:])
+    return accumulator
+
+
+class _VocabularyAccumulatorCoder(_BaseKVCoder):
   """Coder for vocabulary accumulators."""
 
   def __init__(self, input_dtype=tf.string.name):
     self._input_dtype = tf.dtypes.as_dtype(input_dtype)
-    self._lengths_prefix_format = 'qq'
-    self._lengths_prefix_length = struct.calcsize(self._lengths_prefix_format)
+    super(_VocabularyAccumulatorCoder, self).__init__()
 
   def encode_cache(self, accumulator):
     token, value = accumulator
@@ -475,19 +545,11 @@ class _VocabularyAccumulatorCoder(CacheCoder):
           for a in (value.count, value.mean, value.variance, value.weight)
       ]
     value = tf.compat.as_bytes(json.dumps(value))
-    len_token, len_value = len(token), len(value)
-    return struct.pack(
-        '{}{}s{}s'.format(self._lengths_prefix_format, len_token, len_value),
-        len_token, len_value, token, value)
+    return super(_VocabularyAccumulatorCoder, self).encode_cache((token, value))
 
   def decode_cache(self, encoded_accumulator):
-    (len_token, len_value) = struct.unpack_from(
-        self._lengths_prefix_format,
-        encoded_accumulator[:self._lengths_prefix_length])
-    accumulator = struct.unpack_from(
-        '{}s{}s'.format(len_token, len_value),
-        encoded_accumulator[self._lengths_prefix_length:])
-
+    accumulator = super(_VocabularyAccumulatorCoder, self).decode_cache(
+        encoded_accumulator)
     token, value = accumulator
     if self._input_dtype is not tf.string:
       token = json.loads(tf.compat.as_text(token))

tensorflow_transform/analyzers.py
@@ -2038,7 +2038,11 @@ class CovarianceCombiner(analyzer_nodes.Combiner):
     expected_cross_terms = sum_product / count
     expected_terms = sum_vectors / count
 
-    return [expected_cross_terms - np.outer(expected_terms, expected_terms)]
+    return [
+        np.ndarray.astype(
+            expected_cross_terms - np.outer(expected_terms, expected_terms),
+            self._numpy_dtype)
+    ]
 
   def output_tensor_infos(self):
     return [
@@ -2111,7 +2115,9 @@ class PCACombiner(CovarianceCombiner):
     sum_product, sum_vectors, count = accumulator
     expected_cross_terms = sum_product / count
     expected_terms = sum_vectors / count
-    cov = expected_cross_terms - np.outer(expected_terms, expected_terms)
+    cov = np.ndarray.astype(
+        expected_cross_terms - np.outer(expected_terms, expected_terms),
+        self._numpy_dtype)
     vals, vecs = np.linalg.eigh(cov)
     sorted_vecs = vecs[:, np.argsort(vals)[::-1]]
     if self._output_dim is None:

tensorflow_transform/beam/cached_impl_test.py
@@ -353,6 +353,7 @@ class CachedImplTest(test_case.TransformTestCase):
 
   def tearDown(self):
     self._context.__exit__()
+    super(CachedImplTest, self).tearDown()
 
   def test_single_phase_mixed_analyzer_run_once(self):
     span_0_key = 'span-0'
@@ -501,6 +502,7 @@ class CachedImplTest(test_case.TransformTestCase):
 
       _ = tft.bucketize(inputs['x'], 2, name='bucketize')
 
+      y_cov = tft.covariance(tf.expand_dims(inputs['y'], axis=1), tf.float32)
       return {
           'x_min':
               tft.min(inputs['x'], name='x') + tf.zeros_like(inputs['x']),
@@ -510,6 +512,8 @@ class CachedImplTest(test_case.TransformTestCase):
               tft.min(inputs['y'], name='y') + tf.zeros_like(inputs['y']),
           'y_mean':
               tft.mean(inputs['y'], name='y') + tf.zeros_like(inputs['y']),
+          'y_cov':
+              tf.math.reduce_sum(y_cov) + tf.zeros_like(inputs['y']),
           's_integerized':
               tft.compute_and_apply_vocabulary(
                   inputs['s'],
@@ -591,6 +595,7 @@ class CachedImplTest(test_case.TransformTestCase):
               'x_min': -2.0,
               'y_mean': 1.0,
               'y_min': -4.0,
+              'y_cov': 25.0,
               's_integerized': 0,
           },
           {
@@ -598,6 +603,7 @@ class CachedImplTest(test_case.TransformTestCase):
               'x_min': -2.0,
               'y_mean': 1.0,
               'y_min': -4.0,
+              'y_cov': 25.0,
               's_integerized': 2,
           },
       ]
@@ -611,7 +617,7 @@ class CachedImplTest(test_case.TransformTestCase):
 
       for key in input_data_dict:
         self.assertIn(key, cache_output)
-        self.assertEqual(7, len(cache_output[key]))
+        self.assertEqual(8, len(cache_output[key]))
 
     tf_transform_output = tft.TFTransformOutput(transform_fn_dir)
     vocab1_path = tf_transform_output.vocabulary_file_by_name('vocab1')
@@ -620,7 +626,7 @@ class CachedImplTest(test_case.TransformTestCase):
     # 4 from analyzing 2 spans, and 2 from transform.
     self.assertEqual(_get_counter_value(p.metrics, 'num_instances'), 8)
     self.assertEqual(_get_counter_value(p.metrics, 'cache_entries_decoded'), 0)
-    self.assertEqual(_get_counter_value(p.metrics, 'cache_entries_encoded'), 14)
+    self.assertEqual(_get_counter_value(p.metrics, 'cache_entries_encoded'), 16)
     self.assertEqual(_get_counter_value(p.metrics, 'saved_models_created'), 2)
 
     with _TestPipeline() as p:
@@ -668,7 +674,7 @@ class CachedImplTest(test_case.TransformTestCase):
 
     # Only 2 from transform.
     self.assertEqual(_get_counter_value(p.metrics, 'num_instances'), 2)
-    self.assertEqual(_get_counter_value(p.metrics, 'cache_entries_decoded'), 14)
+    self.assertEqual(_get_counter_value(p.metrics, 'cache_entries_decoded'), 16)
     self.assertEqual(_get_counter_value(p.metrics, 'cache_entries_encoded'), 0)
 
     # The root CreateSavedModel is optimized away because the data doesn't get