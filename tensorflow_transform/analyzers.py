This CL: - adds default values for analyzers that use NumPyCombiner (sum, count, min, max),  CovarianceCombiner (covariance) and PCACombiner (pca). This allows correct processing of empty inputs; - adjusts the behavior of scale_by_min_max mapper: if the analysis dataset was empty or contained a single distinct value and the transform dataset is not empty, the transform dataset will be scaled using a sigmoid function; - changes the default pickling behavior of QuantilesCombiner to ignore _graph_state.
PiperOrigin-RevId: 320611883

tensorflow_transform/analyzers.py
@@ -211,59 +211,78 @@ class NumPyCombiner(analyzer_nodes.Combiner):
 
   Args:
     fn: The numpy function representing the reduction to be done.
+    default_accumulator_value: The default value each accumulator entry is
+      initialized to.
     output_dtypes: The numpy dtype to cast each output to.
-    output_shapes: The shapes of the outputs.
+    output_shapes: List of tuples representing the shapes of the outputs or
+      Nones if the shapes are not fully defined.
   """
 
-  def __init__(self, fn, output_dtypes, output_shapes):
+  def __init__(self, fn, default_accumulator_value, output_dtypes,
+               output_shapes):
     self._fn = fn
+    self._default_accumulator_value = default_accumulator_value
+    self._default_sub_accumulator = np.array(default_accumulator_value)
     self._output_dtypes = output_dtypes
+    if not all(
+        isinstance(shape, (tuple, type(None))) for shape in output_shapes):
+      raise TypeError('Expected all tuples or Nones, but got %r' %
+                      output_shapes)
     self._output_shapes = output_shapes
 
-  # TODO(b/34792459): merge_accumulators and extract_output assume that not all
-  # accumulator(s) are None.  This only works when .without_defaults() is
-  # used but even in that case it is an implementation detail of Beam that we
-  # should not be relying on.  Instead we should use 0 or +-inf depending on the
-  # accumulator. Invoking self._fn(()) might also be a good way of determining
-  # the default (works for some but not all fns).
   def create_accumulator(self):
-    return None
+    return [
+        self._create_sub_accumulator(shape)
+        for shape in self._output_shapes
+    ]
+
+  def _create_sub_accumulator(self, shape):
+    # Returns a default subaccumulator of the given shape if it's fully defined
+    # and a 0-dim default array otherwise.
+    if shape is None:
+      return self._default_sub_accumulator
+    else:
+      return np.full(shape, self._default_accumulator_value)
 
   def add_input(self, accumulator, batch_values):
     # TODO(b/112414577): Go back to accepting only a single input.
     # See comment in _numeric_combine.
-    reduced_values = batch_values
-    if accumulator is None:
-      return reduced_values
-    else:
-      return [
-          self._fn((sub_accumulator, reduced_value), axis=0)
-          for sub_accumulator, reduced_value
-          in zip(accumulator, reduced_values)]
+    result = []
+    for sub_accumulator, batch_value in zip(accumulator, batch_values):
+      # Discard `sub_accumulator` that is default for an output with an unknown
+      # or a scalar shape. Note that `np.array_equal` below does at most
+      # per-element comparison of 0-dim arrays since `_default_sub_accumulator`
+      # is a 0-dim array, and `np.array_equal` exits early on a shape mismatch.
+      if np.array_equal(sub_accumulator, self._default_sub_accumulator):
+        result.append(batch_value)
+      else:
+        result.append(self._fn((sub_accumulator, batch_value), axis=0))
+    return result
 
   def merge_accumulators(self, accumulators):
-    non_empty_accumulators = [
-        accumulator for accumulator in accumulators if accumulator is not None
-    ]
-    if non_empty_accumulators:
-      return [
-          # numpy's sum, min, max, etc functions operate on array-like objects,
-          # but not arbitrary iterables. Convert the provided sub_accumulators
-          # into a list.
-          self._fn(list(sub_accumulators), axis=0)
-          for sub_accumulators in zip(*non_empty_accumulators)]
-    else:
-      return None
+    result = []
+    for sub_accumulators in zip(*accumulators):
+      # Filter out subaccumulators that are default for an unknown or a scalar
+      # output shape. Note that `np.array_equal` below does at most per-element
+      # comparison of 0-dim arrays since `_default_sub_accumulator` is a 0-dim
+      # array, and `np.array_equal` exits early on a shape mismatch.
+      non_default_sub_accumulators = [
+          sub_accumulator for sub_accumulator in sub_accumulators
+          if not np.array_equal(sub_accumulator, self._default_sub_accumulator)
+      ]
+      if non_default_sub_accumulators:
+        result.append(self._fn(non_default_sub_accumulators, axis=0))
+      else:
+        result.append(self._default_sub_accumulator)
+    return result
 
   def extract_output(self, accumulator):
-    if accumulator is None:
-      return None
-    else:
-      # For each output, cast that output to the specified type.  Note there
-      # will be one output for each input tensor to the analyzer.
-      return [sub_accumulator.astype(output_dtype)
-              for sub_accumulator, output_dtype
-              in zip(accumulator, self._output_dtypes)]
+    # For each output, cast that output to the specified type. Note there
+    # will be one output for each input tensor to the analyzer.
+    return [
+        sub_accumulator.astype(output_dtype) for sub_accumulator, output_dtype
+        in zip(accumulator, self._output_dtypes)
+    ]
 
   def output_tensor_infos(self):
     return [
@@ -288,6 +307,7 @@ def _get_output_shape_from_input(x):
 # with a single combiner.
 def _numeric_combine(inputs,
                      fn,
+                     default_accumulator_value,
                      reduce_instance_dims=True,
                      output_dtypes=None,
                      key=None,
@@ -298,6 +318,9 @@ def _numeric_combine(inputs,
     inputs: A list of tensors, which will be independently reduced.
     fn: A function to reduce tensors across instances/batches, to get a single
         output.
+    default_accumulator_value: The default scalar value that each accumulator
+        entry is initialized to. Must be properly processed by the reduction
+        function.
     reduce_instance_dims: By default collapses the batch and instance dimensions
         to arrive at a single scalar output. If False, only collapses the batch
         dimension and outputs a vector of the same shape as the input.
@@ -321,6 +344,8 @@ def _numeric_combine(inputs,
   for x in inputs:
     if not isinstance(x, tf.Tensor):
       raise TypeError('Expected a Tensor, but got %r' % x)
+  if not np.isscalar(default_accumulator_value):
+    raise TypeError('Expected a scalar, but got %r' % default_accumulator_value)
 
   if output_dtypes is None:
     output_dtypes = [x.dtype for x in inputs]
@@ -329,9 +354,13 @@ def _numeric_combine(inputs,
     output_shapes = [() for _ in inputs]
   else:
     # Reducing over batch dimensions.
-    output_shapes = [x.get_shape() for x in inputs]
-  combiner = NumPyCombiner(
-      fn, [dtype.as_numpy_dtype for dtype in output_dtypes], output_shapes)
+    output_shapes = [
+        (tuple(x.get_shape()) if x.get_shape().is_fully_defined() else None)
+        for x in inputs
+    ]
+  combiner = NumPyCombiner(fn, default_accumulator_value,
+                           [dtype.as_numpy_dtype for dtype in output_dtypes],
+                           output_shapes)
   if key is None:
     return _apply_cacheable_combiner(combiner, *inputs)
 
@@ -420,8 +449,14 @@ def _min_and_max(x, reduce_instance_dims=True, name=None):
     x_batch_minus_min, x_batch_max = tf_utils.reduce_batch_minus_min_and_max(
         x, reduce_instance_dims)
 
+    default_accumulator_value = (-np.inf if x.dtype.is_floating
+                                 else -output_dtype.max)
+
     minus_x_min, x_max = _numeric_combine(  # pylint: disable=unbalanced-tuple-unpacking
-        [x_batch_minus_min, x_batch_max], combine_fn, reduce_instance_dims)
+        inputs=[x_batch_minus_min, x_batch_max],
+        fn=combine_fn,
+        default_accumulator_value=default_accumulator_value,
+        reduce_instance_dims=reduce_instance_dims)
     return tf.cast(0 - minus_x_min, output_dtype), tf.cast(x_max, output_dtype)
 
 
@@ -480,10 +515,14 @@ def _min_and_max_per_key(x, key, reduce_instance_dims=True,
     key_vocab, x_batch_minus_min, x_batch_max = (
         tf_utils.reduce_batch_minus_min_and_max_per_key(x, key))
 
+    default_accumulator_value = (-np.inf if x.dtype.is_floating
+                                 else -output_dtype.max)
+
     key_values = _numeric_combine(  # pylint: disable=unbalanced-tuple-unpacking
-        [x_batch_minus_min, x_batch_max],
-        combine_fn,
-        reduce_instance_dims,
+        inputs=[x_batch_minus_min, x_batch_max],
+        fn=combine_fn,
+        default_accumulator_value=default_accumulator_value,
+        reduce_instance_dims=reduce_instance_dims,
         key=key_vocab,
         key_vocabulary_filename=key_vocabulary_filename)
 
@@ -545,8 +584,12 @@ def sum(x, reduce_instance_dims=True, name=None):  # pylint: disable=redefined-b
     else:
       x = tf.reduce_sum(input_tensor=x, axis=0)
     output_dtype, sum_fn = _sum_combine_fn_and_dtype(x.dtype)
-    return _numeric_combine([x], sum_fn, reduce_instance_dims,
-                            [output_dtype])[0]
+    return _numeric_combine(
+        inputs=[x],
+        fn=sum_fn,
+        default_accumulator_value=0,
+        reduce_instance_dims=reduce_instance_dims,
+        output_dtypes=[output_dtype])[0]
 
 
 @common.log_api_use(common.ANALYZER_COLLECTION)
@@ -671,7 +714,12 @@ def count_per_key(key, key_vocabulary_filename=None, name=None):
 
     output_dtype, sum_fn = _sum_combine_fn_and_dtype(tf.int64)
     numeric_combine_result = _numeric_combine(
-        [batch_counts], sum_fn, True, [output_dtype], key=batch_keys,
+        inputs=[batch_counts],
+        fn=sum_fn,
+        default_accumulator_value=0,
+        reduce_instance_dims=True,
+        output_dtypes=[output_dtype],
+        key=batch_keys,
         key_vocabulary_filename=key_vocabulary_filename)
 
     if key_vocabulary_filename is not None:
@@ -1535,8 +1583,18 @@ class QuantilesCombiner(analyzer_nodes.Combiner):
     if not self._always_return_num_quantiles and self._num_features > 1:
       raise NotImplementedError(
           'Elementwise quantiles requires same boundary count.')
-    self._tf_config = None    # Assigned in initialize_local_state().
-    self._graph_state = None  # Lazily created in _get_graph_state().
+    # Assigned in initialize_local_state().
+    self._tf_config = None
+    # Lazily created in _get_graph_state(). It is explicitly reset to None in a
+    # pickled version of QuantilesCombiner.
+    self._graph_state = None
+
+  def __getstate__(self):
+    # Changes default pickling behavior to ignore self._graph_state. Note that
+    # default unpickling behavior is consistent with the current __init__ logic.
+    state = self.__dict__.copy()
+    state['_graph_state'] = None
+    return state
 
   def initialize_local_state(self, tf_config):
     """Called by the CombineFnWrapper's __init__ method.
@@ -2058,14 +2116,19 @@ def _quantiles_per_key(x, key, num_buckets, epsilon, name=None):
 class CovarianceCombiner(analyzer_nodes.Combiner):
   """Combines the PCollection to compute the biased covariance matrix."""
 
-  def __init__(self, numpy_dtype=np.float64, output_shape=None):
-    """Store the dtype for np arrays/matrices for precision."""
+  def __init__(self, output_shape, numpy_dtype=np.float64):
+    """Store the dtype and shape for np arrays/matrices for precision."""
     self._output_shape = output_shape
     self._numpy_dtype = numpy_dtype
 
   def create_accumulator(self):
     """Create an accumulator with all zero entries."""
-    return None
+    return [
+        np.zeros((self._output_shape[0], self._output_shape[0]),
+                 self._numpy_dtype),
+        np.zeros((self._output_shape[0],), self._numpy_dtype),
+        np.zeros((), self._numpy_dtype)
+    ]
 
   def add_input(self, accumulator, batch_values):
     """Compute sum of input cross-terms, sum of inputs, and count.
@@ -2099,32 +2162,21 @@ class CovarianceCombiner(analyzer_nodes.Combiner):
     batch_sum = np.array(np.sum(batch_value, axis=0), self._numpy_dtype)
     batch_count = np.shape(batch_value)[0]
 
-    if accumulator is None:
-      return [batch_cross_terms, batch_sum, batch_count]
-    else:
-      sum_product, sum_vectors, count = accumulator
-      return [sum_product + batch_cross_terms,
-              sum_vectors + batch_sum,
-              count + batch_count]
+    sum_product, sum_vectors, count = accumulator
+
+    return [
+        sum_product + batch_cross_terms, sum_vectors + batch_sum,
+        count + batch_count
+    ]
 
   def merge_accumulators(self, accumulators):
     """Sums values in each accumulator entry."""
-    accumulators = [
-        accumulator for accumulator in accumulators if accumulator is not None
+    products, vectors, counts = zip(*accumulators)
+    return [
+        np.sum(products, axis=0),
+        np.sum(vectors, axis=0),
+        np.sum(counts, axis=0)
     ]
-    if accumulators:
-      # Because each accumulator contains multiple arrays of different
-      # dimensions, the np.sum operation must be explicitly used across the
-      # entries within each accumulator. np.sum(list(accumulators)) does not
-      # work.
-      sum_product = np.sum(
-          [accumulator[0] for accumulator in accumulators], axis=0)
-      sum_vectors = np.sum(
-          [accumulator[1] for accumulator in accumulators], axis=0)
-      count = np.sum([accumulator[2] for accumulator in accumulators], axis=0)
-      return [sum_product, sum_vectors, count]
-    else:
-      return None
 
   def extract_output(self, accumulator):
     """Run covariance logic on sum_product, sum of input vectors, and count.
@@ -2144,9 +2196,10 @@ class CovarianceCombiner(analyzer_nodes.Combiner):
     """
 
     sum_product, sum_vectors, count = accumulator
+    if count == 0:
+      return [np.zeros(self._output_shape, self._numpy_dtype)]
     expected_cross_terms = sum_product / count
     expected_terms = sum_vectors / count
-
     return [
         np.ndarray.astype(
             expected_cross_terms - np.outer(expected_terms, expected_terms),
@@ -2193,18 +2246,16 @@ def covariance(x, dtype, name=None):
     shape = (input_dim, input_dim)
 
     (result,) = _apply_cacheable_combiner(
-        CovarianceCombiner(dtype.as_numpy_dtype, shape), x)
+        CovarianceCombiner(shape, dtype.as_numpy_dtype), x)
     return result
 
 
 class PCACombiner(CovarianceCombiner):
   """Compute PCA of accumulated data using the biased covariance matrix."""
 
-  def __init__(self, output_dim=None, numpy_dtype=np.float64,
-               output_shape=None):
-    """Store pca output dimension, and dtype for precision."""
-    super(PCACombiner, self).__init__(
-        numpy_dtype=numpy_dtype, output_shape=output_shape)
+  def __init__(self, output_shape, output_dim=None, numpy_dtype=np.float64):
+    """Store pca output dimension, shape and dtype for precision."""
+    super(PCACombiner, self).__init__(output_shape, numpy_dtype=numpy_dtype)
     self._output_dim = output_dim
 
   def extract_output(self, accumulator):
@@ -2223,6 +2274,11 @@ class PCACombiner(CovarianceCombiner):
       A list containing a matrix of shape (input_dim, output_dim).
     """
     sum_product, sum_vectors, count = accumulator
+    if count == 0:
+      # In this case all eigenvalues==0 and we output (possibly truncated) basis
+      # vectors. Note that if _output_dim is None, then M is set to N in np.eye.
+      return [np.eye(N=self._output_shape[0], M=self._output_dim,
+                     dtype=self._numpy_dtype)]
     expected_cross_terms = sum_product / count
     expected_terms = sum_vectors / count
     cov = np.ndarray.astype(
@@ -2323,7 +2379,7 @@ def pca(x, output_dim, dtype, name=None):
     shape = (input_dim, output_dim)
 
     (result,) = _apply_cacheable_combiner(
-        PCACombiner(output_dim, dtype.as_numpy_dtype, shape), x)
+        PCACombiner(shape, output_dim, dtype.as_numpy_dtype), x)
     return result
 
 

tensorflow_transform/analyzers_test.py
@@ -32,7 +32,10 @@ _NP_TYPES = (np.float32, np.float64, np.int32, np.int64)
 _SUM_TEST = dict(
     testcase_name='Sum',
     combiner=analyzers.NumPyCombiner(
-        np.sum, output_dtypes=[np.int64], output_shapes=[(None,)]),
+        fn=np.sum,
+        default_accumulator_value=0,
+        output_dtypes=[np.int64],
+        output_shapes=[None]),
     batches=[
         (np.array([1, 2, 3, 4, 5, 6]),),
         (np.array([1, 2, 3, 4, 5, 6]),),
@@ -43,7 +46,10 @@ _SUM_TEST = dict(
 _SUM_SCALAR_TEST = dict(
     testcase_name='SumScalar',
     combiner=analyzers.NumPyCombiner(
-        np.sum, output_dtypes=[np.int64], output_shapes=[(None,)]),
+        fn=np.sum,
+        default_accumulator_value=0,
+        output_dtypes=[np.int64],
+        output_shapes=[None]),
     batches=[
         (np.array(1),),
         (np.array(2),),
@@ -54,7 +60,10 @@ _SUM_SCALAR_TEST = dict(
 _SUM_OF_SIZE_ZERO_TENSORS_TEST = dict(
     testcase_name='SumOfSizeZeroTensors',
     combiner=analyzers.NumPyCombiner(
-        np.sum, output_dtypes=[np.int64], output_shapes=[(None, None)]),
+        fn=np.sum,
+        default_accumulator_value=0,
+        output_dtypes=[np.int64],
+        output_shapes=[None]),
     batches=[
         (np.array([]),),
         (np.array([]),),
@@ -64,7 +73,8 @@ _SUM_OF_SIZE_ZERO_TENSORS_TEST = dict(
 
 _COVARIANCE_SIZE_ZERO_TENSORS_TEST = dict(
     testcase_name='CovarianceSizeZeroTensors',
-    combiner=analyzers.CovarianceCombiner(numpy_dtype=np.float64),
+    combiner=analyzers.CovarianceCombiner(output_shape=(0, 0),
+                                          numpy_dtype=np.float64),
     batches=[
         (np.empty((1, 0)),),
         (np.empty((2, 0)),),
@@ -74,7 +84,8 @@ _COVARIANCE_SIZE_ZERO_TENSORS_TEST = dict(
 
 _COVARIANCE_WITH_DEGENERATE_COVARIANCE_MATRIX_TEST = dict(
     testcase_name='CovarianceWithDegenerateCovarianceMatrix',
-    combiner=analyzers.CovarianceCombiner(numpy_dtype=np.float64),
+    combiner=analyzers.CovarianceCombiner(output_shape=(3, 3),
+                                          numpy_dtype=np.float64),
     batches=[
         (np.array([[0, 0, 1]]),),
         (np.array([[4, 0, 1], [2, -1, 1]]),),
@@ -87,7 +98,8 @@ _COVARIANCE_WITH_DEGENERATE_COVARIANCE_MATRIX_TEST = dict(
 
 _COVARIANCE_WITH_LARGE_NUMBERS_TEST = dict(
     testcase_name='CovarianceWithLargeNumbers',
-    combiner=analyzers.CovarianceCombiner(numpy_dtype=np.float64),
+    combiner=analyzers.CovarianceCombiner(output_shape=(2, 2),
+                                          numpy_dtype=np.float64),
     batches=[
         (np.array([[2e15, 0], [1e15, 0]]),),
         (np.array([[-2e15, 0], [-1e15, 0]]),),
@@ -97,7 +109,8 @@ _COVARIANCE_WITH_LARGE_NUMBERS_TEST = dict(
 
 _PCA_WITH_DEGENERATE_COVARIANCE_MATRIX_TEST = dict(
     testcase_name='PCAWithDegenerateCovarianceMatrix',
-    combiner=analyzers.PCACombiner(numpy_dtype=np.float64),
+    combiner=analyzers.PCACombiner(output_shape=(3, 3),
+                                   numpy_dtype=np.float64),
     batches=[
         (np.array([[0, 0, 1]]),),
         (np.array([[4, 0, 1], [2, -1, 1]]),),

tensorflow_transform/beam/cached_impl_test.py
@@ -636,6 +636,7 @@ class CachedImplTest(tft_unit.TransformTestCase):
 
     span_0_key = analyzer_cache.DatasetKey('span-0')
     span_1_key = analyzer_cache.DatasetKey('span-1')
+    span_2_key = analyzer_cache.DatasetKey('span-2')
 
     def preprocessing_fn(inputs):
 
@@ -669,7 +670,8 @@ class CachedImplTest(tft_unit.TransformTestCase):
         'label': tf.io.FixedLenFeature([], tf.int64),
     }
     input_data_dict = {
-        span_0_key: [{
+        span_0_key: [],
+        span_1_key: [{
             'x': -2,
             'y': 1,
             's': 'a',
@@ -690,7 +692,7 @@ class CachedImplTest(tft_unit.TransformTestCase):
             's': u'ȟᎥ𝒋ǩľḿꞑȯ𝘱𝑞𝗋𝘴'.encode('utf-8'),
             'label': 1,
         }],
-        span_1_key: [{
+        span_2_key: [{
             'x': 12,
             'y': 1,
             's': u'ȟᎥ𝒋ǩľḿꞑȯ𝘱𝑞𝗋𝘴'.encode('utf-8'),
@@ -731,7 +733,7 @@ class CachedImplTest(tft_unit.TransformTestCase):
         feature_spec,
         input_data_dict,
         preprocessing_fn,
-        datasets_to_transform=[span_1_key],
+        datasets_to_transform=[span_2_key],
         expected_transform_data=expected_transformed_data,
         transform_fn_output_dir=transform_fn_dir,
         use_tfxio=use_tfxio)
@@ -745,10 +747,12 @@ class CachedImplTest(tft_unit.TransformTestCase):
     self.AssertVocabularyContents(vocab1_path, expected_vocabulary_contents)
 
     p = first_run_result.pipeline
-    # 4 from analyzing 2 spans, and 2 from transform.
+    # 6 from analyzing 3 spans, and 2 from transform.
     self.assertMetricsCounterEqual(p.metrics, 'num_instances', 8)
     self.assertMetricsCounterEqual(p.metrics, 'cache_entries_decoded', 0)
-    self.assertMetricsCounterEqual(p.metrics, 'cache_entries_encoded', 16)
+    # 8 entries for each of 3 spans. Note that default values for the empty span
+    # are also encoded.
+    self.assertMetricsCounterEqual(p.metrics, 'cache_entries_encoded', 24)
     self.assertMetricsCounterEqual(p.metrics, 'saved_models_created',
                                    _SINGLE_PHASE_NUM_SAVED_MODELS)
 
@@ -758,7 +762,7 @@ class CachedImplTest(tft_unit.TransformTestCase):
         input_data_dict,
         preprocessing_fn,
         should_read_cache=True,
-        datasets_to_transform=[span_1_key],
+        datasets_to_transform=[span_2_key],
         expected_transform_data=expected_transformed_data,
         transform_fn_output_dir=transform_fn_dir,
         use_tfxio=use_tfxio)
@@ -772,7 +776,7 @@ class CachedImplTest(tft_unit.TransformTestCase):
     p = second_run_result.pipeline
     # Only 2 from transform.
     self.assertMetricsCounterEqual(p.metrics, 'num_instances', 2)
-    self.assertMetricsCounterEqual(p.metrics, 'cache_entries_decoded', 16)
+    self.assertMetricsCounterEqual(p.metrics, 'cache_entries_decoded', 24)
     self.assertMetricsCounterEqual(p.metrics, 'cache_entries_encoded', 0)
 
     # The root CreateSavedModel is optimized away because the data doesn't get

tensorflow_transform/beam/impl_test.py
@@ -1025,13 +1025,13 @@ class BeamImplTest(tft_unit.TransformTestCase):
     input_metadata = tft_unit.metadata_from_feature_spec(
         {'x': tf.io.FixedLenFeature([], tf.float32)})
     expected_data = [{
-        'x_scaled': 5
+        'x_scaled': 9.8201379
     }, {
-        'x_scaled': 5
+        'x_scaled': 9.8201379
     }, {
-        'x_scaled': 5
+        'x_scaled': 9.8201379
     }, {
-        'x_scaled': 5
+        'x_scaled': 9.8201379
     }]
     expected_metadata = tft_unit.metadata_from_feature_spec(
         {'x_scaled': tf.io.FixedLenFeature([], tf.float32)})
@@ -1074,16 +1074,16 @@ class BeamImplTest(tft_unit.TransformTestCase):
         'y': tf.io.FixedLenFeature([], tf.float32)
     })
     expected_data = [{
-        'x_scaled': 5,
+        'x_scaled': 9.8201379,
         'y_scaled': 0
     }, {
-        'x_scaled': 5,
+        'x_scaled': 9.8201379,
         'y_scaled': 0
     }, {
-        'x_scaled': 5,
+        'x_scaled': 9.8201379,
         'y_scaled': 10
     }, {
-        'x_scaled': 5,
+        'x_scaled': 9.8201379,
         'y_scaled': 10
     }]
     expected_metadata = tft_unit.metadata_from_feature_spec({
@@ -1111,6 +1111,37 @@ class BeamImplTest(tft_unit.TransformTestCase):
                                             expected_metadata)
     self.assertTrue(
         'output_min must be less than output_max' in str(context.exception))
+
+  def testScaleMinMaxWithEmptyInputs(self):
+    # x is repeated `multiple` times to test elementwise mapping.
+    multiple = 3
+
+    def preprocessing_fn(inputs):
+      return {
+          'x_scaled':
+              tft.scale_by_min_max(inputs['x']),
+          'x_scaled_elementwise':
+              tft.scale_by_min_max(
+                  tf.tile(inputs['x'], [1, multiple]), elementwise=True)
+      }
+
+    input_data = []
+    input_metadata = tft_unit.metadata_from_feature_spec(
+        {'x': tf.io.FixedLenFeature([1], tf.float32)})
+    test_data = [{'x': [100]}, {'x': [1]}, {'x': [12]}]
+    expected_data = [{'x_scaled': [v], 'x_scaled_elementwise': [v] * multiple}
+                     for v in [1., 0.7310585, 0.9999938]]
+    expected_metadata = tft_unit.metadata_from_feature_spec({
+        'x_scaled': tf.io.FixedLenFeature([1], tf.float32),
+        'x_scaled_elementwise': tf.io.FixedLenFeature([multiple], tf.float32)
+    })
+    self.assertAnalyzeAndTransformResults(
+        input_data,
+        input_metadata,
+        preprocessing_fn,
+        expected_data,
+        expected_metadata,
+        test_data=test_data)
 
   @tft_unit.named_parameters(*_SCALE_TO_Z_SCORE_TEST_CASES)
   def testScaleToZScore(self, input_data, output_data, elementwise):
@@ -1471,6 +1502,37 @@ class BeamImplTest(tft_unit.TransformTestCase):
     self.assertAnalyzeAndTransformResults(input_data, input_metadata,
                                           preprocessing_fn, expected_data,
                                           expected_metadata)
+
+  def testScaleToZScoreWithEmptyInputs(self):
+    # x is repeated `multiple` times to test elementwise mapping.
+    multiple = 3
+
+    def preprocessing_fn(inputs):
+      return {
+          'x_scaled':
+              tft.scale_to_z_score(inputs['x']),
+          'x_scaled_elementwise':
+              tft.scale_to_z_score(
+                  tf.tile(inputs['x'], [1, multiple]), elementwise=True)
+      }
+
+    input_data = []
+    input_metadata = tft_unit.metadata_from_feature_spec(
+        {'x': tf.io.FixedLenFeature([1], tf.float32)})
+    test_data = [{'x': [100]}, {'x': [1]}, {'x': [12]}]
+    expected_data = [{'x_scaled': [v], 'x_scaled_elementwise': [v] * multiple}
+                     for v in [100, 1, 12]]
+    expected_metadata = tft_unit.metadata_from_feature_spec({
+        'x_scaled': tf.io.FixedLenFeature([1], tf.float32),
+        'x_scaled_elementwise': tf.io.FixedLenFeature([multiple], tf.float32)
+    })
+    self.assertAnalyzeAndTransformResults(
+        input_data,
+        input_metadata,
+        preprocessing_fn,
+        expected_data,
+        expected_metadata,
+        test_data=test_data)
 
   def testMeanAndVar(self):
     def analyzer_fn(inputs):
@@ -2061,6 +2123,68 @@ class BeamImplTest(tft_unit.TransformTestCase):
     self.assertAnalyzerOutputs(
         input_data, input_metadata, analyzer_fn, expected_outputs)
 
+  @tft_unit.named_parameters(*tft_unit.cross_named_parameters(
+      [
+          dict(testcase_name='int64', input_dtype=tf.int64),
+          dict(testcase_name='float32', input_dtype=tf.float32)
+      ],
+      [
+          dict(testcase_name='scalar', input_shape=[]),
+          dict(testcase_name='ND', input_shape=[2, 3])
+      ],
+      [
+          dict(testcase_name='elementwise', reduce_instance_dims=False),
+          dict(testcase_name='not_elementwise', reduce_instance_dims=True)
+      ]))
+  def testNumericAnalyzersWithEmptyInputs(self, input_dtype, input_shape,
+                                          reduce_instance_dims):
+
+    def analyzer_fn(inputs):
+      return {
+          'min':
+              tft.min(inputs['a'], reduce_instance_dims=reduce_instance_dims),
+          'max':
+              tft.max(inputs['a'], reduce_instance_dims=reduce_instance_dims),
+          'sum':
+              tft.sum(inputs['a'], reduce_instance_dims=reduce_instance_dims),
+          'size':
+              tft.size(inputs['a'], reduce_instance_dims=reduce_instance_dims),
+          'mean':
+              tft.mean(inputs['a'], reduce_instance_dims=reduce_instance_dims),
+          'var':
+              tft.var(inputs['a'], reduce_instance_dims=reduce_instance_dims),
+      }
+
+    input_data = []
+    canonical_dtype = tft_unit.canonical_numeric_dtype(input_dtype)
+    input_metadata = tft_unit.metadata_from_feature_spec({
+        'a':
+            tf.io.FixedLenFeature(input_shape, canonical_dtype)
+    })
+    input_val_dtype = input_dtype.as_numpy_dtype
+    output_shape = [] if reduce_instance_dims else input_shape
+    output_dtype = canonical_dtype.as_numpy_dtype
+    default_min = np.inf if input_dtype.is_floating else canonical_dtype.max
+    default_max = -np.inf if input_dtype.is_floating else canonical_dtype.min
+    expected_outputs = {
+        'min': np.full(output_shape, default_min, output_dtype),
+        'max': np.full(output_shape, default_max, output_dtype),
+        'sum': np.full(output_shape, 0, output_dtype),
+        'size': np.full(output_shape, 0, np.int64),
+        'mean': np.full(output_shape, 0, np.float32),
+        'var': np.full(output_shape, 0, np.float32),
+    }
+    self.assertAnalyzerOutputs(
+        input_data,
+        input_metadata,
+        analyzer_fn,
+        expected_outputs,
+        test_data=[{
+            'a': np.zeros(input_shape, input_val_dtype)
+        }, {
+            'a': np.ones(input_shape, input_val_dtype)
+        }])
+
   def testNumericMeanWithSparseTensorReduceFalseOverflow(self):
 
     def analyzer_fn(inputs):
@@ -2356,6 +2480,22 @@ class BeamImplTest(tft_unit.TransformTestCase):
     self.assertAnalyzerOutputs(
         input_data, input_metadata, analyzer_fn, expected_outputs)
 
+  def testCovarianceOneDimensionWithEmptyInputs(self):
+    def analyzer_fn(inputs):
+      return {'y': tft.covariance(inputs['x'], dtype=tf.float32)}
+
+    input_data = []
+    input_metadata = tft_unit.metadata_from_feature_spec(
+        {'x': tf.io.FixedLenFeature([1], tf.float32)})
+    test_data = [{'x': [1]}, {'x': [2]}]
+    expected_outputs = {'y': np.array([[0]], dtype=np.float32)}
+    self.assertAnalyzerOutputs(
+        input_data,
+        input_metadata,
+        analyzer_fn,
+        expected_outputs,
+        test_data=test_data)
+
   def testPCAThreeToTwoDimensions(self):
     def analyzer_fn(inputs):
       return {'y': tft.pca(inputs['x'], 2, dtype=tf.float32)}
@@ -2367,6 +2507,23 @@ class BeamImplTest(tft_unit.TransformTestCase):
     expected_outputs = {'y': np.array([[1, 0], [0, 1], [0, 0]], np.float32)}
     self.assertAnalyzerOutputs(
         input_data, input_metadata, analyzer_fn, expected_outputs)
+
+  def testPCAThreeToTwoDimensionsWithEmptyInputs(self):
+    def analyzer_fn(inputs):
+      return {'y': tft.pca(inputs['x'], 2, dtype=tf.float32)}
+
+    input_data = []
+    test_data = [{'x': x} for x in
+                 [[0, 0, 1], [4, 0, 1], [2, -1, 1], [2, 1, 1]]]
+    input_metadata = tft_unit.metadata_from_feature_spec(
+        {'x': tf.io.FixedLenFeature([3], tf.float32)})
+    expected_outputs = {'y': np.array([[1, 0], [0, 1], [0, 0]], np.float32)}
+    self.assertAnalyzerOutputs(
+        input_data,
+        input_metadata,
+        analyzer_fn,
+        expected_outputs,
+        test_data=test_data)
 
   class _SumCombiner(beam.PTransform):
 

tensorflow_transform/beam/tft_unit.py
@@ -114,6 +114,7 @@ class TransformTestCase(test_case.TransformTestCase):
                             input_metadata,
                             analyzer_fn,
                             expected_outputs,
+                            test_data=None,
                             desired_batch_size=None,
                             use_tfxio=False,
                             input_data_is_tfxio_format=False):
@@ -133,6 +134,11 @@ class TransformTestCase(test_case.TransformTestCase):
         dimension and broadcast across this batch dimension.
       expected_outputs: A dict whose keys are the same as those of the output of
         `analyzer_fn` and whose values are convertible to an ndarrays.
+      test_data: (optional) If this is provided then instead of calling
+        AnalyzeAndTransformDataset with input_data, this function will call
+        AnalyzeDataset with input_data and TransformDataset with test_data.
+        Must be provided if the input_data is empty. test_data should also
+        conform to input_metadata.
       desired_batch_size: (Optional) A batch size to batch elements by. If not
         provided, a batch size will be computed automatically.
       use_tfxio: If True, invoke AnalyzeAndTransformDataset using the new API
@@ -178,10 +184,15 @@ class TransformTestCase(test_case.TransformTestCase):
 
       return result
 
-    # Create test dataset by repeating the first instance a number of times.
-    num_test_instances = 3
-    test_data = [input_data[0]] * num_test_instances
-    expected_data = [expected_outputs] * num_test_instances
+    if input_data and not test_data:
+      # Create test dataset by repeating the first instance a number of times.
+      num_test_instances = 3
+      test_data = [input_data[0]] * num_test_instances
+      expected_data = [expected_outputs] * num_test_instances
+    else:
+      # Ensure that the test dataset is specified and is not empty.
+      assert test_data
+      expected_data = [expected_outputs] * len(test_data)
     expected_metadata = metadata_from_feature_spec({
         key: tf.io.FixedLenFeature(value.shape, tf.as_dtype(value.dtype))
         for key, value in six.iteritems(expected_outputs)

tensorflow_transform/mappers.py
@@ -138,6 +138,8 @@ def scale_by_min_max(x,
 
   Returns:
     A `Tensor` containing the input column scaled to [output_min, output_max].
+    If the analysis dataset is empty or contains a singe distinct value, then
+    `x` is scaled using a sigmoid function.
 
   Raises:
     ValueError: If output_min, output_max have the wrong order.
@@ -183,7 +185,9 @@ def scale_by_min_max_per_key(x,
 
   Returns:
     A `Tensor`  or `SparseTensor` containing the input column scaled to
-    [output_min, output_max] on a per-key basis if a key is provided.
+    [output_min, output_max] on a per-key basis if a key is provided. If the
+    analysis dataset is empty or contains a single distinct value, then `x` is
+    scaled using a sigmoid function.
 
   Raises:
     ValueError: If output_min, output_max have the wrong order.
@@ -240,18 +244,19 @@ def _scale_by_min_max_internal(x, key, output_min, output_max, elementwise,
       max_x_value = tf.gather(max_x_value, x.indices[:, 1])
     x_values = x.values
 
-  x_shape = tf.shape(input=x_values)
-
-  # If min==max, the result will be the mean of the requested range.
-  # Note that both the options of tf.where are computed, which means that this
-  # will compute unused NaNs.
+  # If min>=max, then the corresponding input to the min_and_max analyzer either
+  # was empty and the analyzer returned default values, or contained only one
+  # distinct value. In this case we scale x by applying a sigmoid function which
+  # is continuous, increasing and maps (-inf, inf) -> (0, 1). Its output is
+  # then projected on the requested range. Note that both the options of
+  # tf.where are computed, which means that this will compute unused NaNs.
   numerator = tf.cast(x_values, min_x_value.dtype) - min_x_value
   where_cond = min_x_value < max_x_value
   where_cond = tf.cast(
       tf.zeros_like(numerator) + tf.cast(where_cond, numerator.dtype),
       dtype=tf.bool)
   scaled_result = tf.where(where_cond, numerator / (max_x_value - min_x_value),
-                           tf.fill(x_shape, 0.5))
+                           tf.math.sigmoid(x_values))
 
   return compose_result_fn((scaled_result * (output_max - output_min)) +
                            output_min)
@@ -267,7 +272,9 @@ def scale_to_0_1(x, elementwise=False, name=None):
     name: (Optional) A name for this operation.
 
   Returns:
-    A `Tensor` containing the input column scaled to [0, 1].
+    A `Tensor` containing the input column scaled to [0, 1]. If the analysis
+    dataset is empty or contains a single distinct value, then `x` is scaled
+    using a sigmoid function.
   """
   with tf.compat.v1.name_scope(name, 'scale_to_0_1'):
     return _scale_by_min_max_internal(
@@ -296,7 +303,9 @@ def scale_to_0_1_per_key(
     name: (Optional) A name for this operation.
 
   Returns:
-    A `Tensor` containing the input column scaled to [0, 1], per key.
+    A `Tensor` containing the input column scaled to [0, 1], per key. If the
+    analysis dataset is empty or contains a single distinct value, then `x` is
+    scaled using a sigmoid function.
   """
   with tf.compat.v1.name_scope(name, 'scale_to_0_1_per_key'):
     if key is None:
@@ -330,7 +339,9 @@ def scale_to_z_score(x, elementwise=False, name=None, output_dtype=None):
     A `Tensor` or `SparseTensor` containing the input column scaled to mean 0
     and variance 1 (standard deviation 1), given by: (x - mean(x)) / std_dev(x).
     If `x` is floating point, the mean will have the same type as `x`. If `x` is
-    integral, the output is cast to tf.float32.
+    integral, the output is cast to tf.float32. If the analysis dataset is empty
+    or contains a single distinct value, then the input is returned without
+    scaling.
 
     Note that TFLearn generally permits only tf.int64 and tf.float32, so casting
     this scaler's output may be necessary.
@@ -383,7 +394,9 @@ def scale_to_z_score_per_key(x,
 
     That is, for all keys k: (x - mean(x)) / std_dev(x) for all x with key k.
     If `x` is floating point, the mean will have the same type as `x`. If `x` is
-    integral, the output is cast to tf.float32.
+    integral, the output is cast to tf.float32. If the analysis dataset is empty
+    or contains a single distinct value, then the input is returned without
+    scaling.
 
     Note that TFLearn generally permits only tf.int64 and tf.float32, so casting
     this scaler's output may be necessary.