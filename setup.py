8a63bc9eabd156e2cbcab2c6db9dc0a2c8a27537
rtg0795 <36429068+rtg0795@users.noreply.github.com>
2022-02-23 04:00:58 -0500

    TFT 1.4.1 Patch Release (#262)
    * TFT 1.4.1 Patch Release
    
    * Update install.md
    
    * Update setup.py
    
    * Update RELEASE.md

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ def _make_required_install_packages():
   return [
       'absl-py>=0.9,<0.13',
       'apache-beam[gcp]>=2.33,<3',
+      'future',
       'numpy>=1.16,<1.20',
       'protobuf>=3.13,<4',
       'pyarrow>=1,<6',
