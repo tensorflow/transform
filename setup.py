2f754207690ab40f29ba1d912c29373438a10852
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-08-15 16:48:00 -0700

    Add `pytest` as an extra requirement

--- setup.py
+++ setup.py
@@ -19,98 +19,100 @@ from setuptools import setup
 
 
 def select_constraint(default, nightly=None, git_master=None):
-  """Select dependency constraint based on TFX_DEPENDENCY_SELECTOR env var."""
-  selector = os.environ.get('TFX_DEPENDENCY_SELECTOR')
-  if selector == 'UNCONSTRAINED':
-    return ''
-  elif selector == 'NIGHTLY' and nightly is not None:
-    return nightly
-  elif selector == 'GIT_MASTER' and git_master is not None:
-    return git_master
-  else:
-    return default
+    """Select dependency constraint based on TFX_DEPENDENCY_SELECTOR env var."""
+    selector = os.environ.get("TFX_DEPENDENCY_SELECTOR")
+    if selector == "UNCONSTRAINED":
+        return ""
+    elif selector == "NIGHTLY" and nightly is not None:
+        return nightly
+    elif selector == "GIT_MASTER" and git_master is not None:
+        return git_master
+    else:
+        return default
 
 
 # Get version from version module.
-with open('tensorflow_transform/version.py') as fp:
-  globals_dict = {}
-  exec(fp.read(), globals_dict)  # pylint: disable=exec-used
-__version__ = globals_dict['__version__']
+with open("tensorflow_transform/version.py") as fp:
+    globals_dict = {}
+    exec(fp.read(), globals_dict)  # pylint: disable=exec-used
+__version__ = globals_dict["__version__"]
 
 
 def _make_required_install_packages():
-  # Make sure to sync the versions of common dependencies (absl-py, numpy, and
-  # protobuf) with TF and pyarrow version with tfx-bsl.
-  return [
-      'absl-py>=0.9,<2.0.0',
-      'apache-beam[gcp]>=2.53,<3;python_version>="3.11"',
-      'apache-beam[gcp]>=2.47,<3;python_version<"3.11"',
-      'numpy>=1.22.0',
-      'protobuf>=4.25.2,<5;python_version>="3.11"',
-      'protobuf>=3.20.3,<5;python_version<"3.11"',
-      'pyarrow>=10,<11',
-      'pydot>=1.2,<2',
-      'tensorflow'
-      + select_constraint(
-          default='>=2.15,<2.16',
-          nightly='>=2.16.0.dev',
-          git_master='@git+https://github.com/tensorflow/tensorflow@master',
-      ),
-      'tensorflow-metadata'
-      + select_constraint(
-          default='>=1.15.0,<1.16.0',
-          nightly='>=1.16.0.dev',
-          git_master='@git+https://github.com/tensorflow/metadata@master',
-      ),
-      'tfx-bsl'
-      + select_constraint(
-          default='>=1.15.1,<1.16.0',
-          nightly='>=1.16.0.dev',
-          git_master='@git+https://github.com/tensorflow/tfx-bsl@master',
-      ),
-  ]
+    # Make sure to sync the versions of common dependencies (absl-py, numpy, and
+    # protobuf) with TF and pyarrow version with tfx-bsl.
+    return [
+        "absl-py>=0.9,<2.0.0",
+        'apache-beam[gcp]>=2.53,<3;python_version>="3.11"',
+        'apache-beam[gcp]>=2.47,<3;python_version<"3.11"',
+        "numpy>=1.22.0",
+        'protobuf>=4.25.2,<5;python_version>="3.11"',
+        'protobuf>=3.20.3,<5;python_version<"3.11"',
+        "pyarrow>=10,<11",
+        "pydot>=1.2,<2",
+        "tensorflow"
+        + select_constraint(
+            default=">=2.15,<2.16",
+            nightly=">=2.16.0.dev",
+            git_master="@git+https://github.com/tensorflow/tensorflow@master",
+        ),
+        "tensorflow-metadata"
+        + select_constraint(
+            default=">=1.15.0,<1.16.0",
+            nightly=">=1.16.0.dev",
+            git_master="@git+https://github.com/tensorflow/metadata@master",
+        ),
+        "tfx-bsl"
+        + select_constraint(
+            default=">=1.15.1,<1.16.0",
+            nightly=">=1.16.0.dev",
+            git_master="@git+https://github.com/tensorflow/tfx-bsl@master",
+        ),
+    ]
 
 
 # Get the long description from the README file.
-with open('README.md') as fp:
-  _LONG_DESCRIPTION = fp.read()
+with open("README.md") as fp:
+    _LONG_DESCRIPTION = fp.read()
 
 setup(
-    name='tensorflow-transform',
+    name="tensorflow-transform",
     version=__version__,
-    author='Google Inc.',
-    author_email='tensorflow-extended-dev@googlegroups.com',
-    license='Apache 2.0',
+    author="Google Inc.",
+    author_email="tensorflow-extended-dev@googlegroups.com",
+    license="Apache 2.0",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     namespace_packages=[],
     install_requires=_make_required_install_packages(),
-    python_requires='>=3.9,<4',
+    extras_require={"test": ["pytest>=8.0"]},
+    python_requires=">=3.9,<4",
     packages=find_packages(),
     include_package_data=True,
-    package_data={'tensorflow_transform': ['py.typed']},
-    description='A library for data preprocessing with TensorFlow',
+    package_data={"tensorflow_transform": ["py.typed"]},
+    description="A library for data preprocessing with TensorFlow",
     long_description=_LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    keywords='tensorflow transform tfx',
-    url='https://www.tensorflow.org/tfx/transform/get_started',
-    download_url='https://github.com/tensorflow/transform/tags',
-    requires=[])
+    long_description_content_type="text/markdown",
+    keywords="tensorflow transform tfx",
+    url="https://www.tensorflow.org/tfx/transform/get_started",
+    download_url="https://github.com/tensorflow/transform/tags",
+    requires=[],
+)
