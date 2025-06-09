b4f910ca34549946bb32a74e07f45a12b373793c
tf-transform-team <tf-transform-dev@google.com>
2025-06-09 16:01:55 -0700

    Release TF Transform 1.17.0
    PiperOrigin-RevId: 769329902

--- setup.py
+++ setup.py
@@ -44,24 +44,24 @@ def _make_required_install_packages():
   return [
       'absl-py>=0.9,<2.0.0',
       'apache-beam[gcp]>=2.53,<3;python_version>="3.11"',
-      'apache-beam[gcp]>=2.47,<3;python_version<"3.11"',
+      'apache-beam[gcp]>=2.50,<2.51;python_version<"3.11"',
       'numpy>=1.22.0',
-      'protobuf>=4.25.2,<6;python_version>="3.11"',
-      'protobuf>=3.20.3,<5;python_version<"3.11"',
+      'protobuf>=4.25.2,<6.0.0;python_version>="3.11"',
+      'protobuf>=4.21.6,<6.0.0;python_version<"3.11"',
       'pyarrow>=10,<11',
       'pydot>=1.2,<2',
       'tensorflow>=2.17,<2.18',
       'tensorflow-metadata'
       + select_constraint(
-          default='>=1.16.1,<1.17.0',
-          nightly='>=1.17.0.dev',
+          default='>=1.17.1,<1.18.0',
+          nightly='>=1.18.0.dev',
           git_master='@git+https://github.com/tensorflow/metadata@master',
       ),
       'tf_keras>=2',
       'tfx-bsl'
       + select_constraint(
-          default='>=1.16.1,<1.17.0',
-          nightly='>=1.17.0.dev',
+          default='>=1.17.1,<1.18.0',
+          nightly='>=1.18.0.dev',
           git_master='@git+https://github.com/tensorflow/tfx-bsl@master',
       ),
   ]
